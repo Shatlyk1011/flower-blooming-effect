b68f77f216adbdeba5208062e805f561ea5f3ada
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-08-13 18:31:56 +0500

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import ReactLenis from "lenis/react"
-
-//components
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
+'use client'
 
 export default function Home() {
   return (
-    <ReactLenis root options={{ lerp: 0.1 }}>
-      <main>
-        <Hero />
-        <Projects />
-        <div className="h-screen wscreen bg-zinc-800"></div>
-      </main>
-    </ReactLenis>
+    <p>main page</p>
   );
 }
--- src/components/Projects/index.tsx
+++
@@ -1,8 +0,0 @@
-const Projects = () => {
-  return (
-    <section className="bg-cyan-500">
-123123
-    </section>
-  )
-};
-export default Projects
